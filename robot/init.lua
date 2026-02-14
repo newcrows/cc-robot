7d8346ed0044eed729288642239adb6ef21430ad
JM <j.mund@cse-it.net>
2026-02-14 20:47:35 +0100

    implement init_events

--- robot/init.lua
+++ robot/init.lua
@@ -1,3 +1,4 @@
+local initEvents = require((...) .. "/init_events")
 local initInventory = require((...) .. "/init_inventory")
 
 local robot = {}
@@ -6,37 +7,41 @@ local constants = {
     -- stuff that is needed everywhere, like SIDES or possibly RAW_PROPERTIES
 }
 
+--[[
+$ = meta
+# = custom tool/peripheral
+...
+all init calls here, logically separated:
+EVENTS = {
+    addEventListener, removeEventListener, listEventListeners,
+    $dispatchEvent
+}
+PERIPHERALS = {
+    setConstructor, removeConstructor, listConstructors, wrap, wrapUp, wrapDown,
+    $softWrap, $softUnwrap, $wrap, $unwrap
+    #chest, #me_bridge
+}
+INVENTORY = {
+    select, getSelectedName, getItemDetail, getItemCount, getItemSpace, getItemSpaceForUnknown,
+    hasItemCount, hasItemSpace, hasItemSpaceForUnknown, listItems, reserve, free, getReservedItemDetail,
+    getReservedItemCount, getReservedItemSpace, hasReservedItemCount, hasReservedItemSpace, listReservedItems,
+    $listSlots, $getFirstSlot, $selectFirstSlot, $listEmptySlots, $getFirstEmptySlot, $selectFirstEmptySlot,
+    $countItems, $arrangeSlots
+}
+EQUIPMENT = {
+    equip, unequip, getEquipment, hasEquipment, listEquipment,
+    #pickaxe, #axe, #shovel, #sword, #craftingTable
+}
+POSITION = {
+    forward, back, up, down, turnLeft, turnRight, getFuelLevel, refuel, getFuelLimit
+}
+MISC = {
+    place, placeUp, placeDown, drop, dropUp, dropDown, detect, detectUp, detectDown,
+    compare, compareUp, compareDown, suck, suckUp, suckDown, inspect, inspectUp, inspectDown
+}
+]]--
+initEvents(robot, meta, constants)
 initInventory(robot, meta, constants)
--- $ = meta
--- # = custom tool/peripheral
--- ...
--- all init calls here, logically separated:
--- EVENTS = {
---  addEventListener, removeEventListener, listEventListeners,
---  $dispatchEvent
--- }
--- PERIPHERALS = {
---  setConstructor, removeConstructor, listConstructors, wrap, wrapUp, wrapDown,
---  $softWrap, $softUnwrap, $wrap, $unwrap
---  #chest, #me_bridge
--- }
--- INVENTORY = {
---  select, getSelectedName, getItemDetail, getItemCount, getItemSpace, getItemSpaceForUnknown,
---  hasItemCount, hasItemSpace, hasItemSpaceForUnknown, listItems, reserve, free, getReservedItemDetail,
---  getReservedItemCount, getReservedItemSpace, hasReservedItemCount, hasReservedItemSpace, listReservedItems,
---  $listSlots, $getFirstSlot, $selectFirstSlot, $listEmptySlots, $getFirstEmptySlot, $selectFirstEmptySlot,
---  $countItems, $arrangeSlots
--- }
--- EQUIPMENT = {
---  equip, unequip, getEquipment, hasEquipment, listEquipment,
---  #pickaxe, #axe, #shovel, #sword, #craftingTable
--- }
--- POSITION = {
---  forward, back, up, down, turnLeft, turnRight, getFuelLevel, refuel, getFuelLimit
--- }
--- MISC = {
---  place, placeUp, placeDown, drop, dropUp, dropDown, detect, detectUp, detectDown,
---  compare, compareUp, compareDown, suck, suckUp, suckDown, inspect, inspectUp, inspectDown
--- }
+
 
 return robot
---
+++ robot/init_events.lua
@@ -0,0 +1,40 @@
+return function(robot, meta)
+    local listeners = {}
+    local nextId = 1
+
+    function robot.addEventListener(listener)
+        assert(type(listener) == "table", "listener must be a table")
+
+        local id = nextId
+
+        listeners[id] = listener
+        nextId = nextId + 1
+
+        return id
+    end
+
+    function robot.removeEventListener(id)
+        assert(type(id) == "number", "id must be a number")
+
+        listeners[id] = nil
+        return true
+    end
+
+    function robot.listEventListeners()
+        local arr = {}
+
+        for _, listener in pairs(listeners) do
+            table.insert(arr, listener)
+        end
+
+        return arr
+    end
+
+    function meta.dispatchEvent(name, ...)
+        for _, listener in pairs(listeners) do
+            if listener[name] then
+                listener[name](...)
+            end
+        end
+    end
+end
