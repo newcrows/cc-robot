9d06d1df753b7b2167e9055af9cd245ffac47aee
JM <j.mund@cse-it.net>
2026-02-14 21:28:45 +0100

    implement init_misc

---
+++ robot/init_misc.lua
@@ -0,0 +1,73 @@
+return function(robot, meta, constants)
+    function robot.place(name, blocking)
+
+    end
+
+    function robot.placeUp(name, blocking)
+
+    end
+
+    function robot.placeDown(name, blocking)
+
+    end
+
+    function robot.drop(name, count, blocking)
+
+    end
+
+    function robot.dropUp(name, count, blocking)
+
+    end
+
+    function robot.dropDown(name, count, blocking)
+
+    end
+
+    function robot.detect()
+
+    end
+
+    function robot.detectUp()
+
+    end
+
+    function robot.detectDown()
+
+    end
+
+    function robot.compare(name)
+
+    end
+
+    function robot.compareUp(name)
+
+    end
+
+    function robot.compareDown(name)
+
+    end
+
+    function robot.suck(count, blocking)
+
+    end
+
+    function robot.suckUp(count, blocking)
+
+    end
+
+    function robot.suckDown(count, blocking)
+
+    end
+
+    function robot.inspect()
+
+    end
+
+    function robot.inspectUp()
+
+    end
+
+    function robot.inspectDown()
+
+    end
+end
