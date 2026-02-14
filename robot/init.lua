7ea4aef20192d4802c3428d2eb229a09a44b9c9b
JM <j.mund@cse-it.net>
2026-02-14 20:25:16 +0100

    break robot into logical pieces and load via robot/init.lua

---
+++ robot/init.lua
@@ -0,0 +1,42 @@
+local initInventory = require((...) .. "/init_inventory")
+
+local robot = {}
+local meta = {}
+local constants = {
+    -- stuff that is needed everywhere, like SIDES or possibly RAW_PROPERTIES
+}
+
+initInventory(robot, meta, constants)
+-- $ = meta
+-- # = custom tool/peripheral
+-- ...
+-- all init calls here, logically separated:
+-- EVENTS = {
+--  addEventListener, removeEventListener, listEventListeners,
+--  $dispatchEvent
+-- }
+-- PERIPHERALS = {
+--  setConstructor, removeConstructor, listConstructors, wrap, wrapUp, wrapDown,
+--  $softWrap, $softUnwrap, $wrap, $unwrap
+--  #chest, #me_bridge
+-- }
+-- INVENTORY = {
+--  select, getSelectedName, getItemDetail, getItemCount, getItemSpace, getItemSpaceForUnknown,
+--  hasItemCount, hasItemSpace, hasItemSpaceForUnknown, listItems, reserve, free, getReservedItemDetail,
+--  getReservedItemCount, getReservedItemSpace, hasReservedItemCount, hasReservedItemSpace, listReservedItems,
+--  $listSlots, $getFirstSlot, $selectFirstSlot, $listEmptySlots, $getFirstEmptySlot, $selectFirstEmptySlot,
+--  $countItems, $arrangeSlots
+-- }
+-- EQUIPMENT = {
+--  equip, unequip, getEquipment, hasEquipment, listEquipment,
+--  #pickaxe, #axe, #shovel, #sword, #craftingTable
+-- }
+-- POSITION = {
+--  forward, back, up, down, turnLeft, turnRight, getFuelLevel, refuel, getFuelLimit
+-- }
+-- MISC = {
+--  place, placeUp, placeDown, drop, dropUp, dropDown, detect, detectUp, detectDown,
+--  compare, compareUp, compareDown, suck, suckUp, suckDown, inspect, inspectUp, inspectDown
+-- }
+
+return robot
---
+++ robot/init_inventory.lua
@@ -0,0 +1,266 @@
+return function(robot, meta)
+    local function getPhysicalCount(name)
+        local count = 0
+
+        for i = 1, 16 do
+            local detail = turtle.getItemDetail(i)
+
+            if detail and detail.name == name then
+                count = count + detail.count
+            end
+        end
+
+        return count
+    end
+
+    local function getPhysicalSpace(name)
+        local space = 0
+        local emptySlots = 0
+        local stackSize
+
+        for i = 1, 16 do
+            local detail = turtle.getItemDetail(i)
+
+            if detail and detail.name == name then
+                local slotSpace = turtle.getItemSpace(i)
+
+                if not stackSize then
+                    stackSize = detail.count + slotSpace
+                end
+
+                space = space + slotSpace
+            elseif not detail then
+                emptySlots = emptySlots + 1
+            end
+        end
+
+        if not stackSize then
+            stackSize = 64
+        end
+
+        return space + emptySlots * stackSize
+    end
+
+    local function getPhysicalEmptySlots()
+        local emptySlots = 0
+
+        for i = 1, 16 do
+            local detail = turtle.getItemDetail(i)
+
+            if not detail then
+                emptySlots = emptySlots + 1
+            end
+        end
+
+        return emptySlots
+    end
+
+    local function getPhysicalStackSize(name)
+        for i = 1, 16 do
+            local detail = turtle.getItemDetail(i)
+
+            if detail and detail.name == name then
+                return detail.count + turtle.getItemSpace(i)
+            end
+        end
+
+        return 64
+    end
+
+    local function compactPhysical()
+
+    end
+
+    local function getEquipmentCount(name)
+        name = name or meta.selectedName
+
+        local proxy = meta.equipProxies[name]
+
+        if proxy and not proxy.target then
+            return 1
+        end
+
+        return 0
+    end
+
+    local function getEquipmentSpace(name)
+        name = name or meta.selectedName
+
+        local proxy = meta.equipProxies[name]
+
+        if proxy and proxy.target then
+            return 1
+        end
+
+        return 0
+    end
+
+    local function getEmptySlotsNeededForEquipmentAndReserved()
+        local itemsWeNeedSpaceFor = {}
+
+        for _, proxy in pairs(meta.equipProxies) do
+            if proxy.target then
+                itemsWeNeedSpaceFor[proxy.name] = (itemsWeNeedSpaceFor[proxy.name] or 0) + 1
+            end
+        end
+
+        local itemsWeHaveCountOf = {}
+        local itemsWeHaveSpaceFor = {}
+
+        for i = 1, 16 do
+            local detail = turtle.getItemDetail(i)
+
+            if detail then
+                local space = turtle.getItemSpace(i)
+
+                itemsWeHaveCountOf[detail.name] = (itemsWeHaveCountOf[detail.name] or 0) + detail.count
+                itemsWeHaveSpaceFor[detail.name] = (itemsWeHaveSpaceFor[detail.name] or 0) + space
+            end
+        end
+
+        for name, space in pairs(meta.reservedSpaces) do
+            local additionalSpaceWeNeed = math.max(space - (itemsWeHaveCountOf[name] or 0), 0)
+            itemsWeNeedSpaceFor[name] = (itemsWeNeedSpaceFor[name] or 0) + additionalSpaceWeNeed
+        end
+
+        local emptySlotsWeNeed = 0
+
+        for name, space in pairs(itemsWeNeedSpaceFor) do
+            local stackSize = getPhysicalStackSize(name)
+            emptySlotsWeNeed = emptySlotsWeNeed + math.ceil(space / stackSize)
+        end
+
+        return emptySlotsWeNeed
+    end
+
+    function meta.listSlots(name, limit, includeEquipment, includeReserved)
+
+    end
+
+    function meta.getFirstSlot(name, includeEquipment, includeReserved)
+
+    end
+
+    function meta.selectFirstSlot(name, includeEquipment, includeReserved)
+
+    end
+
+    function meta.listEmptySlots(limit, compact)
+
+    end
+
+    function meta.getFirstEmptySlot(compact)
+
+    end
+
+    function meta.selectFirstEmptySlot(compact)
+
+    end
+
+    function meta.countItems(name, includeEquipment, includeReserved)
+
+    end
+
+    function meta.arrangeSlots(layoutFunc)
+        local touchedSlots = {}
+        local setSlot = function(slotId, name, count)
+            if touchedSlots[slotId] then
+                error("slot " .. tostring(slotId) .. " was already set in current arrangeSlots() call")
+            end
+
+            -- do the actual arrangement here
+            return true
+        end
+
+        return layoutFunc(setSlot)
+    end
+
+    function robot.select(name)
+
+    end
+
+    function robot.getSelectedName()
+
+    end
+
+    function robot.getItemDetail(name)
+
+    end
+
+    function robot.getItemCount(name)
+        name = name or meta.selectedName
+        return getPhysicalCount(name) - getEquipmentCount(name) - robot.getReservedItemCount(name)
+    end
+
+    function robot.getItemSpace(name)
+        name = name or meta.selectedName
+        return getPhysicalSpace(name) - getEquipmentSpace(name) - robot.getReservedItemSpace(name)
+    end
+
+    function robot.getItemSpaceForUnknown(stackSize)
+        stackSize = stackSize or 64
+
+        local availableEmptySlots = getPhysicalEmptySlots() - getEmptySlotsNeededForEquipmentAndReserved()
+        return availableEmptySlots * stackSize
+    end
+
+    function robot.hasItemCount(name)
+        return robot.hasItemCount(name) > 0
+    end
+
+    function robot.hasItemSpace(name)
+        return robot.hasItemSpace(name) > 0
+    end
+
+    function robot.hasItemSpaceForUnknown(stackSize)
+        return robot.getItemSpaceForUnknown(stackSize) > 0
+    end
+
+    function robot.listItems()
+
+    end
+
+    function robot.reserve(name, count)
+
+    end
+
+    function robot.free(name, count)
+
+    end
+
+    function robot.getReservedItemDetail(name)
+
+    end
+
+    function robot.getReservedItemCount(name)
+        name = name or meta.selectedName
+
+        if not meta.reservedSpaces[name] then
+            return 0
+        end
+
+        return math.min(getPhysicalCount(name) - getEquipmentCount(name), meta.reservedSpaces[name])
+    end
+
+    function robot.getReservedItemSpace(name)
+        name = name or meta.selectedName
+
+        if not meta.reservedSpaces[name] then
+            return 0
+        end
+
+        return meta.reservedSpaces[name] - robot.getReservedItemCount(name)
+    end
+
+    function robot.hasReservedItemCount(name)
+        return robot.getReservedItemCount(name) > 0
+    end
+
+    function robot.hasReservedItemSpace(name)
+        return robot.getReservedItemSpace(name) > 0
+    end
+
+    function robot.listReservedItems()
+
+    end
+end
