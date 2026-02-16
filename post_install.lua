c52b563b7521b4cb78f9d6a316b6f5cd0e3b13a4
JM <j.mund@cse-it.net>
2026-02-16 19:07:18 +0100

    remove print

--- post_install.lua
+++ post_install.lua
@@ -3,7 +3,6 @@ local destination = args[1]
 local config = args[3]
 
 local function replaceInFile(file, placeholder, replacement)
-    print(file)
     local f = fs.open(file, "r")
     local content = f.readAll()
     f.close()
