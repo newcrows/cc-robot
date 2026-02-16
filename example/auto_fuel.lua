bbbf65232ac6ff0822fa5589bc5c513f123bf8e7
JM <j.mund@cse-it.net>
2026-02-16 20:02:42 +0100

    add examples

---
+++ example/auto_fuel.lua
@@ -0,0 +1,12 @@
+-- snippet assumes a non-full chest in front of the turtle
+local robot = require("%INSTALL_DIR%/robot")
+local chest = robot.wrap()
+
+robot.setAutoFuel("minecraft:coal_block", 64)
+
+local function restockFuel()
+    robot.select("minecraft:coal_block")
+    chest.export(64 - robot.getReservedItemCount())
+end
+
+restockFuel()
