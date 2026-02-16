f841396f5fc3b4f13d7121c40207155575a294e1
JM <j.mund@cse-it.net>
2026-02-16 17:45:45 +0100

    better install

--- install.lua
+++ install.lua
@@ -1,22 +1,4 @@
 local baseUrl = "https://raw.githubusercontent.com/newcrows/cc-robot/refs/heads/main"
-local files = {
-    "robot/init.lua",
-    "robot/init_equipment.lua",
-    "robot/init_events.lua",
-    "robot/init_inventory.lua",
-    "robot/init_misc.lua",
-    "robot/init_peripherals.lua",
-    "robot/init_positioning.lua",
-    "test/init.lua",
-    "test/test_equipment.lua",
-    "test/test_events.lua",
-    "test/test_inventory.lua",
-    "test/test_misc.lua",
-    "test/test_peripherals.lua",
-    "test/test_positioning.lua",
-    --"run_test.lua" write manually depending on download dir
-}
-
 local args = {...}
 local destination = args[1] or "."
 
@@ -36,6 +18,15 @@ local function readableSize(numBytes)
     error("numBytes is too big")
 end
 
+local function downloadConfig()
+    print("download config..")
+
+    local response = http.get(baseUrl .. "/install.config.json")
+    local content = response.readAll()
+
+    return textutils.deserializeJSON(content)
+end
+
 local function downloadFile(file)
     write(file)
 
@@ -50,39 +41,32 @@ local function downloadFile(file)
     localFile.close()
 end
 
-local function downloadFiles()
-    print("downloading files..")
+local function downloadFiles(files)
+    print("download files..")
+
     for _, file in pairs(files) do
         downloadFile(file)
     end
 end
 
-local function createRunTestFile()
-    print("creating " .. destination .. "/run_test.lua..")
+local function install()
+    print("install robot..")
 
-    local rtFile = fs.open(destination .. "/run_test.lua", "w")
-    rtFile.write("local test = require(\"" .. destination .. "/test\")\r\ntest()\r\n")
-    rtFile.close()
-end
-
-local function installRobot()
-    print("installing robot..")
-
-    local ok = pcall(downloadFiles)
+    local ok, config = pcall(downloadConfig)
 
     if not ok then
-        print("..install failed")
+        print("..download config failed")
         return
     end
 
-    ok = pcall(createRunTestFile)
+    ok = pcall(downloadFiles, config.files)
 
     if not ok then
-        print("..install failed")
+        print("..download files failed")
         return
     end
 
     print("..install ok")
 end
 
-installRobot()
+install()
