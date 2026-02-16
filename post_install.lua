95c49905a87daa0a8733d33bd80a028775a6cd1a
JM <j.mund@cse-it.net>
2026-02-16 18:56:13 +0100

    fix install

---
+++ post_install.lua
@@ -0,0 +1,23 @@
+local args = {...}
+local destination = args[1]
+local config = args[3]
+
+local function replaceInFile(file, placeholder, replacement)
+    local f = fs.open(file, "r")
+    local content = f.readAll()
+    f.close()
+
+    local newContent = string.gsub(content, placeholder, replacement)
+
+    f = fs.open(file, "w")
+    f.write(newContent)
+    f.close()
+end
+
+local function replaceInstallDirPlaceholders()
+    for _, file in ipairs(config.files) do
+        replaceInFile(file, "%%INSTALL_DIR%%", destination)
+    end
+end
+
+replaceInstallDirPlaceholders()
