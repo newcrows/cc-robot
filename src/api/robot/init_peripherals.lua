dbd7061e92c61587abcc3b9ee9bb44370bea0716
JM <j.mund@cse-it.net>
2026-02-18 13:15:56 +0100

    restructure project

--- src/api/robot/init_peripherals.lua
+++ src/api/robot/init_peripherals.lua
@@ -44,178 +44,30 @@ return function(robot, meta, constants)
         return proxy
     end
 
-    local function getPhysicalName(side)
-        local inspectFunc = INSPECT_FUNCS[side]
-
-        assert(inspectFunc, "it is NEVER possible to get wrap name for " .. side)
-
-        local ok, detail = inspectFunc()
-        return ok and detail.name or nil
-    end
-
-    local function wrap_0(side, name)
-        local target = peripheral.wrap(side)
-        local constructor = constructors[name]
-
-        if constructor then
-            local opts = {
-                robot = robot,
-                meta = meta,
-                constants = constants,
-                name = name,
-                side = side,
-                target = target
-            }
-
-            target = constructor(opts)
+    local function wrapHelper(side, wrapAs)
+        if side and not SIDES[side] then
+            wrapAs, side = side, nil
         end
 
-        local proxy = createWrapProxy(name, side, target)
-        proxies[side] = proxy
-
-        meta.dispatchEvent("wrap", side, name)
-        return proxy
-    end
-
-    local function wrap(side, wrapAs)
-        wrapAs = wrapAs or getPhysicalName(side)
-
-        if not wrapAs then
-            return nil
-        end
-
-        local equipments = meta.listEquipment()
-
-        for _, equipment in pairs(equipments) do
-            local proxy = equipment.proxy
-
-            if proxy.target and proxy.side == side then
-                if not proxy.unuse() then
-                    error("could not unequip tool on " .. side)
-                end
-            end
-        end
-
-        return wrap_0(side, wrapAs)
-    end
-
-    function meta.getPeripheral(side)
         side = side or SIDES.front
-        local proxy = proxies[side]
 
-        if proxy then
-            return {
-                side = side,
-                proxy = proxy
-            }
-        end
+        -- TODO [JM] peripheral proxy creation here
     end
 
-    function meta.listPeripherals()
-        local arr = {}
-
-        for side, proxy in pairs(proxies) do
-            table.insert(arr, {
-                side = side,
-                proxy = proxy
-            })
-        end
-
-        return arr
-    end
-
-    function meta.setPeripheralConstructor(name, constructor)
-        assert(name, "name must not be nil")
-        assert(type(constructor) == "function", "constructor must be of type function")
-
-        constructors[name] = constructor
-        return true
-    end
-
-    function meta.removePeripheralConstructor(name)
-        assert(name, "name must not be nil")
-
-        constructors[name] = nil
-        return true
-    end
-
-    function meta.getPeripheralConstructorDetail(name)
-        assert(name, "name must not be nil")
-        local constructor = constructors[name]
-
-        if constructor then
-            return {
-                name = name,
-                constructor = constructor
-            }
-        end
-
-        return nil
-    end
-
-    function meta.listPeripheralConstructors()
-        local arr = {}
-
-        for name, constructor in pairs(constructors) do
-            table.insert({
-                name = name,
-                constructor = constructor
-            })
-        end
-
-        return arr
-    end
-
-    function meta.softWrap(side)
-        assert(side, "side must not be nil")
-        local proxy = proxies[side]
-
-        if proxy and not proxy.target then
-            proxy.target = peripheral.wrap(side)
-            meta.dispatchEvent("soft_wrap", side, proxy.name)
-        end
-
-        return true
-    end
-
-    function meta.softUnwrap(side)
-        assert(side, "side must not be nil")
-        local proxy = proxies[side]
-
-        if proxy and proxy.target then
-            proxy.target = nil
-            meta.dispatchEvent("soft_unwrap", side, proxy.name)
-        end
-
-        return true
-    end
-
-    function meta.unwrap(side)
-        assert(side, "side must not be nil")
-        local proxy = proxies[side]
-
-        if proxy then
-            proxies[side] = nil
-            meta.dispatchEvent("unwrap", side, proxy.name)
-        end
+    local function loadConstructors()
+        local dir = "%INSTALL_DIR%/peripherals"
     end
 
     function robot.wrap(side, wrapAs)
-        if side and not SIDES[side] then
-            wrapAs = side
-            side = nil
-        end
-
-        side = side or SIDES.front
-        return wrap(side, wrapAs)
+        return wrapHelper(side, wrapAs)
     end
 
     function robot.wrapUp(wrapAs)
-        return wrap(SIDES.top, wrapAs)
+        return wrapHelper(SIDES.top, wrapAs)
     end
 
     function robot.wrapDown(wrapAs)
-        return wrap(SIDES.bottom, wrapAs)
+        return wrapHelper(SIDES.bottom, wrapAs)
     end
 
     -- TODO [JM] load all custom peripherals from %INSTALL_DIR%/api/peripherals/* here
