a8169c054ef768b30f33c952bc9e777bd6a10bc0
JM <j.mund@cse-it.net>
2026-02-19 02:48:39 +0100

    check in tooling output for later diffing

--- src/post_install.lua
+++ src/post_install.lua
@@ -1,7 +1,7 @@
 local args = { ... }
 -- local flags = args[1]
 local destination = args[2]
--- local branch = args[2]
+-- local branch = args[3]
 local config = args[4]
 
 local function replaceInFile(file, placeholder, replacement)
