9d7a935a4e80af1f6713e5c280c682c0b1717cd2
JM <j.mund@cse-it.net>
2026-02-20 08:30:04 +0100

    update tooling to work with all api paths

--- tooling/extract-funcs.js
+++ tooling/extract-funcs.js
@@ -1,8 +1,25 @@
 const fs = require("fs");
 const nodePath = require("path");
-const inputDir = "../src/api/robot";
+const inputDir = "../src/api";
 
-let globalStrOut = ""
+let globalLocalOut = createToStringOut();
+let globalMetaOut = createToStringOut();
+let globalRobotOut = createToStringOut();
+
+function createToStringOut() {
+  let str = "";
+
+  const out = function (...strings) {
+    const line = strings.join(" ") + "\n";
+    str += line
+  }
+
+  out.toString = function () {
+    return str;
+  }
+
+  return out;
+}
 
 function extract(filename) {
   const outputFile = "./out/" + filename
@@ -12,51 +29,42 @@ function extract(filename) {
   const content = fs.readFileSync(filename, {encoding: "utf-8"});
   const localFuncRegex = /^\s*(local\s+)function.*$/gm;
   const metaFuncRegex = /^\s*function meta.*$/gm;
-  const robotFuncRegex = /^\s*function robot.*$/gm;
 
-  function printMatches(label, content, regex, out) {
+  const apiName = nodePath.relative(inputDir, filename).split('/')[0]
+  const apiRegex = new RegExp(`^\\s*function ${apiName}.*$`, "gm");
+
+  function printMatches(label, content, regex, out, noLabelOut) {
     const matches = content.match(regex);
-    out("----", label, "----");
+    out("----", label.toUpperCase(), "----");
 
     if (matches) {
+      noLabelOut("# " + nodePath.relative("../src", filename));
+
       for (let match of matches) {
-        out(match.trim().replace(/^function /, ""));
+        const text = match.trim().replace(/^function /, "");
+
+        out(text);
+        noLabelOut(text);
       }
+
+      noLabelOut();
     }
 
     out();
   }
 
-  function createToStringOut() {
-    let str = "";
-
-    const out = function (...strings) {
-      const line = strings.join(" ") + "\n";
-      str += line
-    }
-
-    out.toString = function () {
-      return str;
-    }
-
-    return out;
-  }
-
   const strOut = createToStringOut();
 
   try {
-    printMatches("local functions", content, localFuncRegex, strOut);
-    printMatches("meta functions", content, metaFuncRegex, strOut);
-    printMatches("robot functions", content, robotFuncRegex, strOut);
+    printMatches("local functions", content, localFuncRegex, strOut, globalLocalOut);
+    printMatches("meta functions", content, metaFuncRegex, strOut, globalMetaOut);
+    printMatches(apiName + " functions", content, apiRegex, strOut, globalRobotOut);
   } catch (e) {
     console.log("error in", filename);
   }
 
   fs.mkdirSync(nodePath.dirname(outputFile), {recursive: true});
   fs.writeFileSync(outputFile, strOut.toString(), {encoding: "utf-8"});
-
-  globalStrOut += "#MODULE " + filename + "\n\n";
-  globalStrOut += strOut.toString();
 }
 
 function traverse(path, callback) {
@@ -73,4 +81,15 @@ function traverse(path, callback) {
 
 traverse(inputDir, extract);
 
-fs.writeFileSync("./out/summary.txt", globalStrOut.toString(), {encoding: "utf-8"});
+let globalStr = "";
+
+globalStr += "---- local functions ----\n".toUpperCase();
+globalStr += globalLocalOut.toString();
+
+globalStr += "---- meta functions ----\n".toUpperCase();
+globalStr += globalMetaOut.toString();
+
+globalStr += "---- api functions ----\n".toUpperCase();
+globalStr += globalRobotOut.toString();
+
+fs.writeFileSync("./out/summary.txt", globalStr, {encoding: "utf-8"});
