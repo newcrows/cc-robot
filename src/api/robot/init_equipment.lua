8899dbd8436205ff5163b45febd91c12a3324dfe
JM <j.mund@cse-it.net>
2026-02-21 07:15:51 +0100

    make robot.equip() instantly require the equipment

--- src/api/robot/init_equipment.lua
+++ src/api/robot/init_equipment.lua
@@ -251,6 +251,8 @@ return function(robot, meta, constants)
     function robot.equip(name, pinned)
         name = name or robot.getSelectedName()
 
+        meta.requireEquipment(name) -- program doesn't continue beyond the equip call until equipment is present
+
         if type(name) == "table" then
             proxies[name] = name
             name.invalid = nil
