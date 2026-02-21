22d9ac7705e0dbc6de1098f52a8103001bf6d663
JM <j.mund@cse-it.net>
2026-02-21 03:31:26 +0100

    update next_version file

--- src/api/robot/init_core.lua
+++ src/api/robot/init_core.lua
@@ -144,6 +144,8 @@ return function(_, meta)
     --
     -- // let the loop pass and meta.require continues to handle the problem
     -- NOTE: e must still carry e.alreadyWarned, we need that prop!
+    -- NOTE: e.cancelRequire, e.getRequireArgs only exist if the event is a meta.require event
+    --  any other event does not have those (duh)
     function meta.require(check, get, warning)
         if type(check) ~= "function" then
             error("check must be a function", 0)
