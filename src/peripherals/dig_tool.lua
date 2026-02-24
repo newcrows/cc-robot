256ec437ae42bc3c18ae15f2ca1db9ed1cadec8d
JM <j.mund@cse-it.net>
2026-02-24 12:59:41 +0100

    update tooling output

--- src/peripherals/dig_tool.lua
+++ src/peripherals/dig_tool.lua
@@ -6,7 +6,10 @@ return function(_, meta)
             "minecraft:diamond_shovel"
         },
         constructor = function()
-            local function digHelper(digFunc, blocking)
+            local function digHelper(digFunc, query, blocking)
+                -- TODO [JM] only invName matters in the query
+                --  -> if itemName is set, we add the block only to queryInv if match
+                --      if no match, we put it in fallback_inventory (this is coherent with robot.suck())
                 local ok
 
                 local function check()
@@ -15,6 +18,11 @@ return function(_, meta)
 
                 local function tick()
                     ok = digFunc()
+
+                    -- TODO [JM] advanced checks with inspect() and peripheral.getType():full-inventory-sync
+                    -- -> if simple block, meta.updateItemCount(itemName@queryInventory, 1)
+                    --      (or fallback_inventory, if no itemName specified or block doesnt match itemName)
+                    -- -> if type == "inventory" -> do full inventory before after diff after breaking the block
                 end
 
                 meta.try(check, tick, blocking)
@@ -22,14 +30,14 @@ return function(_, meta)
             end
 
             return {
-                dig = function(blocking)
-                    return digHelper(nativeTurtle.dig, blocking)
+                dig = function(query, blocking)
+                    return digHelper(nativeTurtle.dig, query, blocking)
                 end,
-                digUp = function(blocking)
-                    return digHelper(nativeTurtle.digUp, blocking)
+                digUp = function(query, blocking)
+                    return digHelper(nativeTurtle.digUp, query, blocking)
                 end,
-                digDown = function(blocking)
-                    return digHelper(nativeTurtle.digDown, blocking)
+                digDown = function(query, blocking)
+                    return digHelper(nativeTurtle.digDown, query, blocking)
                 end
             }
         end
