04165688d6d329a908f6e6113e05e640e0d6a5c6
JM <j.mund@cse-it.net>
2026-02-25 04:09:09 +0100

    update todos

--- src/peripherals/dig_tool.lua
+++ src/peripherals/dig_tool.lua
@@ -1,4 +1,4 @@
-return function(_, meta)
+return function(robot, meta)
     return {
         names = {
             "minecraft:diamond_pickaxe",
@@ -6,7 +6,7 @@ return function(_, meta)
             "minecraft:diamond_shovel"
         },
         constructor = function()
-            local function digHelper(digFunc, query, blocking)
+            local function digHelper(digFunc, inspectFunc, query, blocking)
                 -- TODO [JM] only invName matters in the query
                 --  -> if itemName is set, we add the block only to queryInv if match
                 --      if no match, we put it in fallback_inventory (this is coherent with robot.suck())
@@ -17,12 +17,21 @@ return function(_, meta)
                 end
 
                 local function tick()
-                    ok = digFunc()
+                    local blockExists, blockDetail = inspectFunc()
 
-                    -- TODO [JM] advanced checks with inspect() and peripheral.getType():full-inventory-sync
-                    -- -> if simple block, meta.updateItemCount(itemName@queryInventory, 1)
-                    --      (or fallback_inventory, if no itemName specified or block doesnt match itemName)
-                    -- -> if type == "inventory" -> do full inventory before after diff after breaking the block
+                    if blockExists then
+                        -- TODO [JM] advanced checks with inspect() and peripheral.getType():full-inventory-sync
+                        -- -> if simple block, meta.updateItemCount(itemName@queryInventory, 1)
+                        --      (or fallback_inventory, if no itemName specified or block doesnt match itemName)
+                        -- -> if type == "inventory" -> do full inventory before after diff after breaking the block
+
+                        ok = digFunc()
+
+                        if ok then
+                            local itemName, invName = meta.parseQuery(query, blockDetail.name)
+                            meta.updateItemCount(itemName .. "@" .. invName, 1)
+                        end
+                    end
                 end
 
                 meta.try(check, tick, blocking)
@@ -31,13 +40,13 @@ return function(_, meta)
 
             return {
                 dig = function(query, blocking)
-                    return digHelper(nativeTurtle.dig, query, blocking)
+                    return digHelper(nativeTurtle.dig, nativeTurtle.inspect, query, blocking)
                 end,
                 digUp = function(query, blocking)
-                    return digHelper(nativeTurtle.digUp, query, blocking)
+                    return digHelper(nativeTurtle.digUp, nativeTurtle.inspectUp, query, blocking)
                 end,
                 digDown = function(query, blocking)
-                    return digHelper(nativeTurtle.digDown, query, blocking)
+                    return digHelper(nativeTurtle.digDown, nativeTurtle.inspectDown, query, blocking)
                 end
             }
         end
