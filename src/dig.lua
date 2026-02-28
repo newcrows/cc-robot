4d96e4d613c31344deca564079d6d6f732e75e67
JM <j.mund@cse-it.net>
2026-02-28 11:40:45 +0100

    add dig script

--- src/dig.lua
+++ src/dig.lua
@@ -90,3 +90,6 @@ digCuboid(width, height, length)
 
 -- go back to start when finished
 robot.moveTo(0, 0, 0, "north")
+
+-- NOTE this script can auto-resume when started as a task (requires GPS and optionally a compass)
+-- run this command in shell: task dig <width> <height> <length> --gps
