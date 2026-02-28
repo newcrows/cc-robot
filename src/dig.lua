1c88e3a9da2d6b6984ed8788584ef99c0f3fdca4
JM <j.mund@cse-it.net>
2026-02-28 11:15:02 +0100

    add dig script

--- src/dig.lua
+++ src/dig.lua
@@ -66,7 +66,7 @@ local function digCuboid(width, height, length)
     -- TODO [JM] implement
 end
 
--- local parse args
+-- parse args
 local args = {...}
 local width = tonumber(args[1])
 local height = tonumber(args[2])
